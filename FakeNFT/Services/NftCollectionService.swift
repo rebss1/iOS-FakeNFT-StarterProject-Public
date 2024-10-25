0e7cc6ff95c4766e8f050241b8ccfbc50673be89
Loshilov Ilya <loshilovilya@yandex.ru>
2024-10-25 15:21:31 +0300

    Edit NftCollectionService

--- FakeNFT/Services/NftCollectionService.swift
+++ FakeNFT/Services/NftCollectionService.swift
@@ -10,7 +10,6 @@ import Foundation
 typealias NftCollectionCompletion = (Result<[NftCollection], Error>) -> Void
 
 protocol NftCollectionService {
-//    func loadNftCollection(id: String, completion: @escaping NftCollectionCompletion)
     func loadNftCollections(completion: @escaping NftCollectionCompletion)
 }
 
@@ -23,24 +22,6 @@ final class NftCollectionServiceImpl: NftCollectionService {
         self.storage = storage
         self.networkClient = networkClient
     }
-
-//    func loadNftCollection(id: String, completion: @escaping NftCollectionCompletion) {
-//        if let collection = storage.getCollection(with: id) {
-//            completion(.success(collection))
-//            return
-//        }
-//
-//        let request = NFTCollectionRequest(id: id)
-//        networkClient.send(request: request, type: NftCollection.self) { [weak storage] result in
-//            switch result {
-//            case .success(let collection):
-//                storage?.saveCollection(collection)
-//                completion(.success(collection))
-//            case .failure(let error):
-//                completion(.failure(error))
-//            }
-//        }
-//    }
     
     func loadNftCollections(completion: @escaping NftCollectionCompletion) {
         let request = NFTCollectionsRequest()
